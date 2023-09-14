58ff510f66805a350446c10b3c509ebb7cc98ffa
tyopoyt <dcrouseIII@gmail.com>
2023-09-14 08:46:08 -0400

    Begin implementing damage calc

--- damage-calc/src/app/components/damage-calculator/damage-calculator.component.ts
+++ damage-calc/src/app/components/damage-calculator/damage-calculator.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Languages } from 'src/app/models/languages';
 import { LanguageService } from 'src/app/services/language.service';
+import { ScreenService } from 'src/app/services/screen.service';
+import { DismissibleColorOption } from '../ui-elements/dismissible/dismissible.component';
 
 @Component({
   selector: 'app-damage-calculator',
@@ -10,13 +12,54 @@ import { LanguageService } from 'src/app/services/language.service';
 })
 export class DamageCalculatorComponent implements OnInit {
 
+  DismissibleColorOption = DismissibleColorOption;
+
   constructor (
     private route: ActivatedRoute,
-    public languageService: LanguageService
+    public languageService: LanguageService,
+    public screenService: ScreenService
   ) {}
   
   ngOnInit() {
     const langParam = this.route.snapshot.paramMap.get('lang') || 'us';
     this.languageService.setLanguage(Languages[langParam])
   }
+
+  attackChange(value: number) {
+
+  }
+  attackImprintChange(value: number) {
+
+  }
+  attackIncreaseChange(value: number) {
+
+  }
+  critDamageChange(value: number) {
+
+  }
+  damageIncreaseChange(value: number) {
+
+  }
+  artifactLevelChange(value: number) {
+
+  }
+  targetDefenseChange(value: number) {
+
+  }
+  targetDefenseIncreaseChange(value: number) {
+
+  }
+  damageReductionChange(value: number) {
+
+  }
+  damageTransferChange(value: number) {
+
+  }
+  casterMaxHPChange(value: number) {
+
+  }
+  molagoraChange(value: number) {
+
+  }
+
 }
