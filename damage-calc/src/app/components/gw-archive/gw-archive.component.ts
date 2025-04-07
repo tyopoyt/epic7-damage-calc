fd11afba7bffcf91bc8990723a857748815d0617
tyopoyt <dcrouseIII@gmail.com>
2025-04-07 13:01:21 -0400

    Begin adding gw archive image tool

---
+++ damage-calc/src/app/components/gw-archive/gw-archive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gw-archive',
+  templateUrl: './gw-archive.component.html',
+  styleUrls: ['./gw-archive.component.scss']
+})
+export class GwArchiveComponent {
+
+}
